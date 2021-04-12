b39b64d6603b5cc1269ef355fc272583ebb5ddff
Lukas Kienast <lukaskienast0@gmail.com>
2021-04-12 21:37:50 +0200

    Revert "Removed Jumphost"
    This reverts commit 9d3d21bb97d26b71c13373432de0bf8974fbf206.

---
+++ jumphost-hardening/README.md
@@ -0,0 +1,6 @@
+# jumphost hardening (In Progress)
+
+An example app with a bastion in a public subnet and a webserver also in public subnet
+
+The bastion is secured by multiple things:
+1. allowing ssh access only to a defined list of ip adresses
---
+++ jumphost-hardening/roles/infrastructure/terraform/variables.tf
@@ -0,0 +1,48 @@
+variable "region" {
+  type        = string
+  description = "The region to deploy to- Replaced by Ansible Playbook on run"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "The cidr_block to set in VPC- Replaced by Ansible Playbook on run"
+}
+
+variable "subnet_public_cidr_block" {
+  type        = string
+  description = "The cidr_block to set in public subnet- Replaced by Ansible Playbook on run"
+}
+
+variable "ip_adresses" {
+  type        = list(string)
+  description = "The ip addresses allowed for ssh access- Replaced by Ansible Playbook on run"
+}
+
+variable "app" {
+  description = "the name of your stack, e.g. \"demo\""
+}
+
+variable "environment" {
+  type        = string
+  description = "the name of your environment, e.g. \"prod\""
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The instancetype to use"
+}
+
+variable "ami_bastion" {
+  type        = string
+  description = "The ami to use for the bastion"
+}
+
+variable "ami_webserver" {
+  type        = string
+  description = "The ami to use for the webserver"
+}
+
+variable "key_name" {
+  type        = string
+  description = "the name of the keypair for ssh"
+}
