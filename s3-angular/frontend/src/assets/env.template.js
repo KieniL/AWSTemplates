ef07f21d88b1e0c37f6cbfa0d3932ada9c9b9faa
Kreidl <lukaskienast0@gmail.com>
2022-01-14 07:22:11 +0100

    removed not needed frontend

--- s3-angular/frontend/src/assets/env.template.js
+++
@@ -1,6 +0,0 @@
-(function(window) {
-  window["env"] = window["env"] || {};
-
-  // Environment variables
-  window["env"]["AWS_BASE_PATH"] = "${AWS_BASE_PATH}";
-})(this);
--- s3-angular/frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  AWS_BASE_PATH: window["env"]["AWS_BASE_PATH"] || "default"
-};
